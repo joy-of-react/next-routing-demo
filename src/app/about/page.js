0a9c97df760b075810c9bed6af1dbea8fba6e904
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-14 17:57:33 -0400

    Initial setup

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <main>
+      <h1>About This Website</h1>
+      <p>It is a website.</p>
+      <p>
+        <a href="/">Return home</a>
+      </p>
+    </main>
+  );
+}
+
+export default AboutPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Home() {
-  return <p>Hello world!</p>;
+  return (
+    <main>
+      <h1>Welcome to my website!</h1>
+      <p>
+        To learn more about this website, visit
+        our <a href="/about">About Page</a>.
+      </p>
+    </main>
+  );
 }
 
 export default Home;
